98d17b01e01a312cfb9bf02d10ce853f036db162
mel_121821 <meganlindsay48@gmail.com>
2022-11-29 07:12:56 -0500

    Create a mouseOver() function to turn divs black when mouse enters. Nest the mouseOver() function into the createDivs() function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //let sketchDiv; 
 
 const sketchPad = document.getElementById('sketchpad');
+let cell = ""
 
 function createDivs(num) {
     for(x = 0; x < num; x++) {
@@ -8,8 +9,9 @@ function createDivs(num) {
         row.className = "row";
         
         for(y = 1; y <= num; y++) {
-            let cell = document.createElement("div");
+            cell = document.createElement("div");
             cell.className = "cell";
+            mouseOver();
             row.appendChild(cell);
         }
         sketchPad.appendChild(row); 
@@ -17,6 +19,12 @@ function createDivs(num) {
 }
 
 createDivs(4);
+
+function mouseOver() {
+    cell.addEventListener('mouseenter', (event) => {
+        event.target.style.backgroundColor = "black";
+    })
+}
 
 //let sketchDiv1x1 = document.createElement('div');
 //document.getElementById('sketchpad').appendChild(sketchDiv1x1)
