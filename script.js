8a0e09f8b19301dc5e091b8de60bbfe03c791d6f
mel_121821 <meganlindsay48@gmail.com>
2022-12-06 19:29:15 -0500

    Create variables for multicolor and greyscale buttons, create a function to toggle button values on and off, add an event listener to the multicolor button and adjust mouseover function to include the multicolor option

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //let sketchDiv; 
 
 const sketchPad = document.getElementById('sketchpad');
+
 const gridSizeButton = document.getElementById('changeSize');
 let userField = document.getElementById('newSize');
 const form = document.getElementById('form')
+
+const multiColorButton = document.getElementById('multiColor');
+const greyScaleButton = document.getElementById('greyScale');
 
 let cell = "";
 let row = "";
@@ -25,10 +29,19 @@ function createDivs(num) {
 
 createDivs(16);
 
+multiColorButton.addEventListener('click', () => {
+    toggle(multiColorButton);
+    console.log(multiColorButton);
+})
+
 
 function mouseOver() {
     cell.addEventListener('mouseenter', (event) => {
-        event.target.style.backgroundColor = 'rgba(' + generateRandomColor() + ',' + generateRandomColor() + ',' + generateRandomColor() + '\)';
+        if (multiColorButton.value === "ON") {
+            event.target.style.backgroundColor = 'rgba(' + generateRandomColor() + ',' + generateRandomColor() + ',' + generateRandomColor() + '\)';
+        } else {
+            event.target.style.backgroundColor = "grey";
+        }
         
     })
 }
@@ -37,7 +50,15 @@ function generateRandomColor() {
     return Math.floor(Math.random() * 255)
 }
 
-
+function toggle(buttonName) {
+    if (buttonName.value === "ON") {
+        buttonName.value = "OFF";
+        buttonName.style.borderColor = "red"
+    } else if (buttonName.value === "OFF") {
+        buttonName.value = "ON"
+        buttonName.style.borderColor = "green"
+    }
+}
 
 //let randomColor = "";
 
